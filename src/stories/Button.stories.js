6e838630a044b3f107eea98828e0958e7a24d254
Stepan Voevodin <life5432@gmail.com>
2025-06-15 22:35:22 +0300

    Fix Button component and story

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,6 @@
 import { fn } from 'storybook/test';
 
-import MyButton from './Button.vue';
+import MyButton from '../components/Button.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
@@ -8,8 +8,12 @@ export default {
   component: MyButton,
   tags: ['autodocs'],
   argTypes: {
-    size: { control: { type: 'select' }, options: ['small', 'medium', 'large'] },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
     backgroundColor: { control: 'color' },
+    onClick: { action: 'clicked' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
