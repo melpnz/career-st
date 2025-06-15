ec29bb0290823db310a8c9b6b74f2d6e8d745f4f
Stepan Voevodin <life5432@gmail.com>
2025-06-15 22:33:18 +0300

    Fix Storybook configuration and dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,7 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
