4551b80c542e161f16deff4a724508cb99d1325e
Stepan Voevodin <life5432@gmail.com>
2025-06-15 22:38:57 +0300

    Remove storybook/test import from Header stories

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,35 +1,14 @@
-import { fn } from 'storybook/test';
-
-import MyHeader from './Header.vue';
+import Header from './Header.vue';
 
 export default {
   title: 'Example/Header',
-  component: MyHeader,
+  component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  render: (args) => ({
-    // Components used in your story `template` are defined in the `components` object
-    components: {
-      MyHeader,
-    },
-    // The story's `args` need to be mapped into the template through the `setup()` method
-    setup() {
-      // Story args can be spread into the returned object
-      return {
-        ...args,
-      };
-    },
-    // Then, the spread values can be accessed directly in the template
-    template: '<my-header :user="user" />',
-  }),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
-  },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
+  argTypes: {
+    onLogin: { action: 'onLogin' },
+    onLogout: { action: 'onLogout' },
+    onCreateAccount: { action: 'onCreateAccount' },
   },
 };
 
@@ -42,7 +21,5 @@ export const LoggedIn = {
 };
 
 export const LoggedOut = {
-  args: {
-    user: null,
-  },
+  args: {},
 };
