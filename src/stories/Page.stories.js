f9d938f50aa1e8f9428ae25cc5958dfb4f72b705
Stepan Voevodin <life5432@gmail.com>
2025-06-15 22:40:44 +0300

    Remove storybook/test import from Page stories

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,34 +1,24 @@
-import { expect, userEvent, within } from 'storybook/test';
-
-import MyPage from './Page.vue';
+import Page from './Page.vue';
 
 export default {
   title: 'Example/Page',
-  component: MyPage,
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+  component: Page,
+  tags: ['autodocs'],
+  argTypes: {
+    onLogin: { action: 'onLogin' },
+    onLogout: { action: 'onLogout' },
+    onCreateAccount: { action: 'onCreateAccount' },
   },
 };
 
-export const LoggedOut = {};
-
-// More on component testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn = {
-  render: () => ({
-    components: {
-      MyPage,
+  args: {
+    user: {
+      name: 'Jane Doe',
     },
-    template: '<my-page />',
-  }),
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = canvas.getByRole('button', { name: /Log in/i });
-    await expect(loginButton).toBeInTheDocument();
-    await userEvent.click(loginButton);
-    await expect(loginButton).not.toBeInTheDocument();
-
-    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
-    await expect(logoutButton).toBeInTheDocument();
   },
+};
+
+export const LoggedOut = {
+  args: {},
 };
