1d1b35b326aae41791b6a0d64cf5344d61fb7ff6
Stepan Voevodin <life5432@gmail.com>
2025-06-15 22:26:44 +0300

    Initial commit with Storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag",
+  },
+  "staticDirs": ["../public"],
+  "viteFinal": async (config) => {
+    return {
+      ...config,
+      base: process.env.NODE_ENV === 'production' ? '/career-st/' : '/',
+    };
+  },
+};
+export default config;
